3b3dd16ff5db3a1669f34e14fd28ed892ba600e6
Adam-Tidball <abtidball@gmail.com>
2024-09-30 15:54:29 -0700

    Revert "formatting and updated about me section"
    This reverts commit 2ec6cd957f51ee28797c749ace6295cb6fdcc040.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,72 +3,44 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="row-start-1 w-full lg:w-4/5 ">
-        <div className="flex flex-col sm:flex-row items-center justify-between w-full">
-          <div className="flex flex-col items-center sm:items-start">
-            <h1 className="text-2xl sm:text-3xl font-bold">Adam Tidball</h1>
-            <p className="text-sm mb-4 sm:mb-2">
-              Software Engineering Portfolio
-            </p>
-          </div>
-          <div className="flex items-center">
-            <a
-              className="text-sm sm:text-base text-right flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="mailto:abtidball@gmail.com"
-            >
-            <Image
-              aria-hidden
-              src="/icons/134146_mail_email_icon.png"
-              alt="Email: "
-              width={16}
-              height={16}
-            />
-            abtidball@gmail.com
-            </a>
-          </div>
+      <header className="row-start-1 grid grid-rows-[auto_auto] gap-4 w-2/3 items-center">
+        <div className="items-center sm:items-start py-8">
+          <h1 className="text-2xl sm:text-3xl font-bold text-center">Adam Tidball</h1>
+          <p className="text-sm text-center">
+            Software Engineering Portfolio
+          </p>
+        </div>
+        <div className="flex flex-col sm:flex-row justify-center gap-6 sm:gap-12 items-center">
+          <a
+            className="px-4 py-2 rounded-full border border-solid border-black/[.08] hover:bg-black/[.08]"
+            href="#AboutMe"
+          >
+            About Me
+          </a>
+          <a
+            className="px-4 py-2 rounded-full border border-solid border-black/[.08] hover:bg-black/[.08]"
+            href="#Projects"
+          >
+            Projects
+          </a>
+          <a
+            className="px-4 py-2 rounded-full border border-solid border-black/[.08] hover:bg-black/[.08]"
+            href="#Footer"
+          >
+            Contact Info
+          </a>
         </div>
       </header>
 
-      <main className="flex flex-col gap-8 row-start-2 w-full lg:w-2/3">
-        <div className="flex flex-col gap-4 items-center">
-          <p className="text-4xl sm:text-5xl font-bold text-center">
-            Hello, I'm Adam!
-          </p>
-          <p className="text-lg sm:text-xl text-center mb-2 sm:mb-4">
-            Welcome to my software portfolio.
-          </p>
-          <div className="flex flex-col sm:flex-row justify-center gap-6 sm:gap-12 items-center">
-            <a
-              className="px-4 py-2 rounded-full border border-solid border-black/[.08] hover:bg-black/[.08]"
-              href="#AboutMe"
-            >
-              About Me
-            </a>
-            <a
-              className="px-4 py-2 rounded-full border border-solid border-black/[.08] hover:bg-black/[.08]"
-              href="#Projects"
-            >
-              Projects
-            </a>
-            <a
-              className="px-4 py-2 rounded-full border border-solid border-black/[.08] hover:bg-black/[.08]"
-              href="#Footer"
-            >
-              Contact Info
-            </a>
-          </div>
-        </div>
-
-        <section id="AboutMe" className="flex flex-col gap-4 mt-8">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <section id="AboutMe" className="flex flex-col gap-4">
           <h2 className="text-lg font-bold">About Me</h2>
           <p>
-            I graduated with a degree in Software Engineering from the University of Victoria, specializing in data mining and analysis, artificial intelligence, and machine learning. I have a passion for problem-solving and leveraging the power of technology to create innovative solutions. I am constantly inspired by the impact software can have in transforming industries and improving lives.
-            < br />
-            < br />
-            Throughout my academic journey, I have developed a strong foundation in various programming languages and software engineering principles. In addition to my education, I’ve had the opportunity to gain practical experience through 16 months of co-op work including as a Quality Assurance Engineer, IT support, and as an entrepreneur creating an NFT project. These experiences have provided me with hands-on exposure to real-world challenges and helped me develop a versatile skill set.
-            < br />
-            < br />
-            Outside of tech, I’m passionate about hockey, golf, and chess—each offering its own unique challenges to tackle. I thrive on solving problems, whether they’re on the ice, on the course, or in the software world, and I’m always ready for the next big challenge.
+            I am a software engineer with a passion for creating and maintaining
+            software that is both efficient and user-friendly. I have a
+            background in full-stack development, with experience in both
+            front-end and back-end technologies. I am always looking for new
+            challenges and opportunities to learn and grow as a developer.
           </p>
         </section>
         <section id="Projects" className="flex flex-col gap-4">
@@ -107,14 +79,13 @@ export default function Home() {
         </div>
         <div className="col-start-1 sm:col-start-1 sm:col-span-2 flex gap-8 flex-wrap items-center justify-center mt-8">
           <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="mailto:abtidball@gmail.com"
+          className="flex items-center gap-2  "
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/portfolio/icons/134146_mail_email_icon.png"
+            src="/icons/134146_mail_email_icon.png"
             alt="Email: "
             width={16}
             height={16}
