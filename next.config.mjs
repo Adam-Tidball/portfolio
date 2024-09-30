9061d6a869084aab5c6e71dfec9b5bf03d77a531
Adam-Tidball <abtidball@gmail.com>
2024-09-29 18:51:59 -0700

    updated github slug

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
      *
      * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
      */
-    basePath: "/adam-tidball.github.io/portfolio/",
+    basePath: "portfolio/",
   
     /**
      * Disable server-based image optimization. Next.js does not support
